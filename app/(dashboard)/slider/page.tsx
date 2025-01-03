e403782b8c76fa6615f98016fac43fda9501ba82
MeledakCik <kakangkasyaf@gmail.com>
2025-01-03 14:22:48 +0800

    update

--- app/(dashboard)/slider/page.tsx
+++
@@ -1,9 +0,0 @@
-import Slider from '@/components/slider/page';
-
-export default function Home() {
-    return (
-        <div className="min-h-screen justify-center items-center">
-            <Slider />
-        </div>
-    );
-}
